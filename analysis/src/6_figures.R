a72595f77ded4dddbb291f937823d52c90bb0cd5
mcolvin <colvin.mike@gmail.com>
2016-11-11 17:05:07 -0600

    added new plot
    added plots for length-weight and age-length

--- analysis/src/6_figures.R
+++ analysis/src/6_figures.R
@@ -10,3 +10,20 @@ if(n==1)
 	}
 
 }
+
+presPlot()
+plot(Length..mm.~Age,agedat,
+    xlab="Age",ylab="Length(mm)",las=1)
+text(4, 120, 
+    expression(L(Age) ==344  %.%(1- plain(e)^{-0.4 %.%(Age-1.0)})),
+    cex=1.3)
+
+ plot(Weight..g.~Length..mm.,agedat,
+    xlab="Length(mm)",ylab="Weight (g)",las=1)
+text(150, 320, 
+    expression(Weight ==10^{-6.2} %.%Length^{3.61}))
+
+    lm(log10(Weight..g.)~log10(Length..mm.),agedat,subset=
+        Weight..g.>0)
+    
+    log10(agedat$Weight..g.)
